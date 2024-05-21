0518b2ff13352e4289f8cef7585cb3407fbca5f8
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-21 09:42:46 +0530

    added

--- src/components/devicefilteroptions/DeviceFilter.jsx
+++ src/components/devicefilteroptions/DeviceFilter.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import { Container, Form } from 'react-bootstrap';
-import "./deviceFilter.css"
+import React from "react";
+import { Container, Form } from "react-bootstrap";
+import "./deviceFilter.css";
 
-function DeviceFilter({ deviceState, featureState, onDeviceChange, onFeatureChange }) {
+function DeviceFilter({
+  deviceState,
+  featureState,
+  onDeviceChange,
+  onFeatureChange,
+}) {
   const handleDeviceChange = (e) => {
     const { id, checked } = e.target;
     onDeviceChange({ ...deviceState, [id]: checked });
@@ -17,69 +22,69 @@ function DeviceFilter({ deviceState, featureState, onDeviceChange, onFeatureChan
     <div className="position_box">
       <Container>
         {/* Box 1: Device Checkboxes */}
-     <div className='position_outerbox'>
-     <div className='position_secondBox'>
-        <div className='heading_position'>
-        <h5>Devices</h5>
-        </div>
-          <Form className="position_form_sizing">
-            <Form.Group>
-              <Form.Check
-                type="checkbox"
-                label="Mobile"
-                id="mobile"
-                checked={deviceState.mobile}
-                onChange={handleDeviceChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Check
-                type="checkbox"
-                label="Tablet"
-                id="tablet"
-                checked={deviceState.tablet}
-                onChange={handleDeviceChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Check
-                type="checkbox"
-                label="Desktop"
-                id="desktop"
-                checked={deviceState.desktop}
-                onChange={handleDeviceChange}
-              />
-            </Form.Group>
-          </Form>
-        </div>
+        <div className="position_outerbox">
+          <div className="position_secondBox">
+            <div className="heading_position">
+              <h5>Devices</h5>
+            </div>
+            <Form className="position_form_sizing">
+              <Form.Group>
+                <Form.Check
+                  type="checkbox"
+                  label="Mobile"
+                  id="mobile"
+                  checked={deviceState.mobile}
+                  onChange={handleDeviceChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Check
+                  type="checkbox"
+                  label="Tablet"
+                  id="tablet"
+                  checked={deviceState.tablet}
+                  onChange={handleDeviceChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Check
+                  type="checkbox"
+                  label="Desktop"
+                  id="desktop"
+                  checked={deviceState.desktop}
+                  onChange={handleDeviceChange}
+                />
+              </Form.Group>
+            </Form>
+          </div>
 
-        {/* Box 2: Feature Checkboxes */}
-        <div className='position_secondBox' >
-         <div className='heading_position'>
-         <h5>Filter options</h5>
-         </div>
-          <Form className="position_form_sizing"> 
-            <Form.Group>
-              <Form.Check
-                type="checkbox"
-                label="Gatesurvey"
-                id="gatesurvey"
-                checked={featureState.gatesurvey}
-                onChange={handleFeatureChange}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Check
-                type="checkbox"
-                label="Fraud Detection"
-                id="fraudDetection"
-                checked={featureState.fraudDetection}
-                onChange={handleFeatureChange}
-              />
-            </Form.Group>
-          </Form>
+          {/* Box 2: Feature Checkboxes */}
+          <div className="position_secondBox">
+            <div className="heading_position">
+              <h5>Filter options</h5>
+            </div>
+            <Form className="position_form_sizing">
+              <Form.Group>
+                <Form.Check
+                  type="checkbox"
+                  label="Gatesurvey"
+                  id="gatesurvey"
+                  checked={featureState.gatesurvey}
+                  onChange={handleFeatureChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Check
+                  type="checkbox"
+                  label="Fraud Detection"
+                  id="fraudDetection"
+                  checked={featureState.fraudDetection}
+                  onChange={handleFeatureChange}
+                />
+              </Form.Group>
+            </Form>
+          </div>
         </div>
-     </div>
       </Container>
     </div>
   );
